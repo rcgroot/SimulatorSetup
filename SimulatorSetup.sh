647221f8e0f2464179aa02896e762fff15e9c868
Rene de Groot <rene.de.groot@sogeti.nl>
2016-03-01 09:50:23 +0100

    Transform the script to capture app group state

--- SimulatorSetup.sh
+++ SimulatorSetup.sh
@@ -1,23 +1,68 @@
 #!/bin/sh
-#
-# Setup Simulator accounts for testing
-# by overwriting Simulator Library with contents of SimulatorLibraryOverride folder
-#
-# To update SimulatorLibraryOverride manually copy included subdirectories from simulator library folder
-if [[ -d "$PROJECT_DIR" ]]
+if [[ "$1" == "read" || "$1" == "write" ]];
 then
-    echo "Overriding accounts in iPhone Simulator sandbox environment"
-    for device in ~/Library/Developer/CoreSimulator/Devices/*/device.plist; do
-        runtime=$(defaults read "${device}" runtime)
-        devicedir="$(dirname ${device})"
-        libdir="$devicedir/data/Library"
-        if [[ $runtime = com.apple.CoreSimulator.SimRuntime.iOS-8* ]]
-        then
-            echo "Overriding iOS8 accounts in:$libdir"
-            cp -vfR "$PROJECT_DIR/Acceptance Tests/SimulatorLibraryOverride8"/* "$libdir"/
-        fi
-    done
-else
-    echo "PROJECT_DIR is not defined to find override files"
-    exit 1
+  ACTION=$1
+  shift
 fi
+
+while [[ $# > 1 ]]
+do
+  key="$1"
+  case $key in
+      -g|--appgroup)
+      GROUP_PACKAGE="$2"
+      shift # past argument
+      ;;
+      *)
+              # unknown option
+      ;;
+  esac
+  shift # past argument or value
+done
+
+if [[ -z "$ACTION" || -z "$GROUP_PACKAGE" ]]
+then
+  echo "Usage $0 [read|write] -g <(partial) package_id>"
+  exit 1
+fi
+
+for device in ~/Library/Developer/CoreSimulator/Devices/*/device.plist; do
+    runtime=$(defaults read "${device}" runtime)
+    devicedir="$(dirname ${device})"
+    libdir="$devicedir/data/Library"
+    groupsdir="$devicedir/data/Containers/Shared/AppGroup"
+    if [[ $runtime = com.apple.CoreSimulator.SimRuntime.iOS* ]]
+    then
+      VERSION=${runtime#com.apple.CoreSimulator.SimRuntime.iOS-}
+      GROUP_ARCHIVE=AppGroupCopy-${VERSION}.zip
+      if [[ ! -z "$GROUP_PACKAGE" ]]
+      then
+        for group in ${groupsdir}/*
+        do
+          if grep -q $GROUP_PACKAGE "${group}/.com.apple.mobile_container_manager.metadata.plist";
+          then
+            echo "Found for $GROUP_PACKAGE in $group"
+            if [[ "$ACTION" == "read" ]];
+            then
+              if [ -f ${GROUP_ARCHIVE} ]
+              then
+                rm -v ${GROUP_ARCHIVE}
+              fi
+              cd "${group}"
+              zip -vr ${GROUP_ARCHIVE} .
+              cd -
+              mv "${group}"/${GROUP_ARCHIVE} .
+            fi
+            if [[ "$ACTION" == "write" ]];
+            then
+              if [ -f ${GROUP_ARCHIVE} ]
+              then
+                rm -r "${group}"/*
+                unzip ${GROUP_ARCHIVE} -d "${group}"
+              fi
+            fi
+          fi
+        done
+      fi
+    fi
+done
